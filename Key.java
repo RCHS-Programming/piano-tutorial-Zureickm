07af20724440e90d247ddf4d34dcbc8ff92391bf
Zureick <zureickm@gmail.com>
2021-04-19 08:34:42 -0400

    keys

--- Key.java
+++ Key.java
@@ -2,17 +2,33 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, and Greenfoot)
 
 public class Key extends Actor
 {
+    private boolean isDown;
+    private String key;
+    private String sound;
     /**
      * Create a new key.
      */
-    public Key()
-    {
-    }
-
+    public Key(String keyName, String soundFile)        
+   {                
+        key = keyName;                
+        sound = soundFile;        
+      }
     /**
      * Do the action for this key.
      */
     public void act()
     {
+      if ( !isDown && Greenfoot.isKeyDown (key))        
+        {               
+          setImage ("white-key-down.png");                
+          isDown = true;      
+          Greenfoot.playSound (sound);
+        }
+      if ( isDown && !Greenfoot.isKeyDown (key))       
+        {                
+             setImage ("white-key.png");                
+             isDown = false;        
+        }
     }
+
 }
