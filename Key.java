6d64c5b52e088716fdf9f23ae0d07ee6dcfd39cb
Zureick <zureickm@gmail.com>
2021-04-27 08:37:26 -0400

    Whites okay

--- Key.java
+++ Key.java
@@ -5,13 +5,19 @@ public class Key extends Actor
     private boolean isDown;
     private String key;
     private String sound;
+    private String upImage;
+    private String downImage;
     /**
      * Create a new key.
      */
-    public Key(String keyName, String soundFile)        
+    public Key(String keyName, String soundFile, String img1, String img2)        
    {                
         key = keyName;                
         sound = soundFile;        
+        upImage = img1;        
+        downImage = img2;        
+        setImage (upImage);        
+        isDown = false;
       }
     /**
      * Do the action for this key.
--- Piano.java
+++ Piano.java
@@ -8,6 +8,9 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, and Greenfoot)
  */
 public class Piano extends World
 {
+    private String[] whiteKeys ={ "a", "s", "d", "f", "g", "h", "j", "k", "l", ";", "'", "\\" };
+    private  String[] whiteNotes ={ "3c", "3d", "3e", "3f", "3g", "3a", "3b", "4c", "4d", "4e", "4f", "4g" };
+    
     /**
      * Make the piano.
      */
@@ -20,11 +23,13 @@ public class Piano extends World
     }
     public void makekeys()
     {
-      addObject ( new Key  ( "g", "3a.wav"),  300,  180 );
-      for (int i=0; i<12; i++)
-      {            
-        addObject (new Key ("g", "3a.wav"), i*63 + 54, 140);
-      }
+        int i;
+       for (i=0; i < whiteKeys.length; i++)        
+       {                
+           Key key = new Key(whiteKeys[i], whiteNotes[i] + ".wav", "white-key.png", "white-key-down.png");         
+           addObject (key,  54  +  i*63,  140);        
+        }
+    
         
     }
 }
